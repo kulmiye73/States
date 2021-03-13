ee2562337d6c2c2918d110451a7694cd8b05a902
kulmiye73 <kulmiye73@student.rtc.edu>
2021-03-13 00:09:48 -0800

    I change the state.py to zipcode and i imported mysql.connector,flask

--- states.py
+++ states.py
@@ -4,16 +4,16 @@ from sqlalchemy import create_engine
 hostname="127.0.0.1"
 uname="root"
 pwd=""
-dbname="states"
+dbname="zipcodes"
 
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
             .format(host=hostname, db=dbname, user=uname, pw=pwd))
 
-tables = pandas.read_csv(r"C:\Users\ellis\Documents\Populationstate.csv")
+tables = pandas.read_csv(r"F:\RTC\WINTER-2021\CNA 335PROGRAMSCRIPTING F MGNT\WEEK-SIX\zip_code_database.csv")
 
 
 
 connection=engine.connect()
-tables.to_sql('States',con = engine, if_exists = 'replace')
+tables.to_sql('zipcodes',con = engine, if_exists = 'replace')
 connection.close()
 print(tables)
